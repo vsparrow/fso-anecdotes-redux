d50b36d087cf4973fbd13d798b7f35e2d3a7dfe5
vsparrow.com <khan@vsparrow.com>
2020-01-21 19:25:06 +0000

    remove CLEAR_FILTER action, currently unused

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -3,8 +3,8 @@ const filterReducer = (state='',action)=>{
 	switch(action.type){
 		case 'UPDATE_FILTER':
 			return action.filter
-		case 'CLEAR_FILTER':
-			return ''
+		// case 'CLEAR_FILTER':
+		// 	return ''
 		default:
 			return state
 	}
@@ -12,6 +12,6 @@ const filterReducer = (state='',action)=>{
 
 //action creators
 export const updateFilter = (filter)=>({type:'UPDATE_FILTER', filter})
-export const clearFilter = ()=>({type:'CLEAR_FILTER', filter:''})
+// export const clearFilter = ()=>({type:'CLEAR_FILTER', filter:''})
 
 export default filterReducer
